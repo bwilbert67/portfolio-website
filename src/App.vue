73ab53c4e8b2f49d07ce32d2747827f74d29f9ff
bwilbert67 <bwilbert67@gmail.com>
2023-05-31 16:37:48 -0400

    added custom mouse, changed banner links to not have pointer mouse. Underline will signify it is a link

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
+<link-banner></link-banner>
 <ben-home></ben-home>
 </template>
 
 <script>
+import LinkBanner from './components/LinkBanner.vue'
 import BenHome from './views/BenHome.vue'
-
 export default {
   name: 'App',
   components: {
     BenHome,
+    LinkBanner,
   }
 }
 </script>
 
 <style>
-
+body{
+cursor: url("./assets/images/cursor.cur"), auto;
+}
+.mouse-fix{
+  cursor: url("./assets/images/cursor.cur"), auto;
+}
 </style>
---
+++ src/components/LinkBanner.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="banner">
+    <router-link class = "mouse-fix" to="/">Home</router-link>
+    <router-link class = "mouse-fix" to="/about">About Me</router-link>
+    <router-link class = "mouse-fix" to="/projects">Projects</router-link>
+    <router-link class = "mouse-fix" to="/blog">Blog</router-link>
+    <router-link class = "mouse-fix" to="/contact">Contact</router-link>
+    <router-link class = "mouse-fix" to="/scratch-pad">Scratch Pad</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LinkBanner',
+};
+</script>
+
+<style scoped>
+.banner {
+  background-color: #f2f2f2;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+}
+a {
+    text-decoration: underline;
+    color: black;
+}
+.mouse-fix{
+  cursor: url("../assets/images/cursor.cur"), auto;
+}
+</style>
