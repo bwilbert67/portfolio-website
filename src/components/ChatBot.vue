a8dab99d12de6b037294bb599b4e783c8b70bc39
bwilbert67 <bwilbert67@gmail.com>
2023-06-19 14:51:08 -0400

    chatbot interface adjustments

--- src/components/ChatBot.vue
+++ src/components/ChatBot.vue
@@ -1,51 +1,74 @@
 <template>
-  <div class="right-side">
-    <div class="text-output">
-      This is place holder text for the really cool chatbot
+  <div>
+    <h1>Chatbot</h1>
+
+    <div id="chat-container">
+      <div v-for="message in messages" :key="message.id" :class="['message', message.type]">
+        {{ message.text }}
+      </div>
     </div>
-    <div class="text-input">
-      <textarea></textarea>
-    </div>
+
+    <input type="text" id="chat-input" v-model="userInput" placeholder="Type your message..." autofocus @keydown.enter="processUserInput" />
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      messages: [],
+      userInput: ''
+    };
+  },
+  methods: {
+    addUserMessage(message) {
+      this.messages.push({ id: Date.now(), text: message, type: 'user-message' });
+    },
+    addBotMessage(message) {
+      this.messages.push({ id: Date.now(), text: message, type: 'bot-message' });
+    },
+    processUserInput() {
+      const userInput = this.userInput.trim();
+      
+      if (userInput) {
+        this.addUserMessage(userInput);
+        // Add your chatbot logic here to generate a response
+        // For example, you can use an API call or predefined responses
+        
+        // Placeholder bot response
+        this.addBotMessage('This is a placeholder bot response.');
+        
+        this.userInput = '';
+      }
+    }
+  }
+};
 </script>
 
-<style>
-.right-side {
-  display: flex;
-  flex-direction: column;
-  justify-content: center; /* Center vertically */
-  align-items: center; /* Center horizontally */
-  height: 90vh;
-  width: 100%;
+<style scoped>
+#chat-container {
+  width: 400px;
+  height: 500px;
+  border: 1px solid #ccc;
+  overflow-y: scroll;
+  padding: 10px;
 }
 
-.text-output {
-  flex-grow: 8;
-  overflow-y: scroll;
-  border: 1px solid #ccc;
-  height: 80vh;
-  width: 80vh;
+.message {
+  margin-bottom: 10px;
 }
 
-.text-input {
-  flex-grow: 2;
-  overflow-y: scroll;
-  border: 1px solid #ccc;
-  height: 10vh;
-  width: 80vh;
+.user-message {
+  background-color: #e6e6e6;
 }
-textarea {
+
+.bot-message {
+  background-color: #f2f2f2;
+}
+
+#chat-input {
   width: 100%;
-  height: 100%;
-  padding: 0.5rem;
-  border: none;
-  outline: none;
-  white-space: pre-wrap;
-  overflow-wrap: break-word;
-  overflow: hidden;
+  padding: 10px;
+  box-sizing: border-box;
 }
 </style>
