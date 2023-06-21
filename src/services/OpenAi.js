d2aee9f1455757a18a0837033e9314efb2ea68d2
bwilbert67 <bwilbert67@gmail.com>
2023-06-21 14:24:18 -0400

    tuning prompt

--- src/services/OpenAi.js
+++ src/services/OpenAi.js
@@ -8,6 +8,8 @@ const http = axios.create({
     'Authorization': `Bearer ${process.env.VUE_APP_API_KEY}`
   }
 });
+
+// Background data for ChatGPT
 const resume = `
 Bennett Wilbert
 bwilbert67@gmail.com | (609) 850-1019 | www.linkedin.com/in/Bennett-L-Wilbert | https://github.com/bwilbert67
@@ -18,38 +20,30 @@ TECHNICAL EXPERIENCE
 ● Meal-Prep Application - Worked with a team of 5 to create a Full Stack Java meal plan application. Users can organize their account by creating recipes, adding recipes to a meal plan, and generating a grocery list based on their meal plan. Created under an Agile methodology with a product owner, daily stand-up, and engineering tickets
 ● Vending Machine Application - Partner programmed to design an object-oriented virtual vending machine program using Java (IntelliJ) that displays inventory/stock from a file, accepts user input for depositing money and making a purchase, gives change, and logs transactions by writing to a file.
 ● PostgreSQL Venmo CLI Project - Partner programmed the backend of a Venmo “clone” project using a PostgreSQL database, Java SpringBoot, and REST API principles. Allows users to send money to others, view their transaction history, and check their account balance. Secured from SQL injection and with Spring Security. Implements integration testing and exception handling to ensure proper functionality.
-● Text-Based Adventure - Designed the database and class structure for a command-line adventure game. Allows users to create a character and play through a story that changes based on their decisions. Utilizes Object Oriented Principles such as inheritance, encapsulation, and polymorphism.
 EDUCATION
 Tech Elevator, Philadelphia, PA January 2023  April 2023 A 14-week full-stack coding bootcamp learning how to create dynamic web-based software systems using Java providing 800+ hours of development education and application.
-      Rowan University, Glassboro, NJ
+  Rowan University, Glassboro, NJ
 Masters of Science in Teaching (2021-2022), GPA: 4.0 Bachelor of Arts in History (2017-2021), GPA: 3.9
 WORK EXPERIENCE
-Clearview Regional School District, Clearview, NJ Long-Term Substitute
-Fall 2017  Spring 2022
-August 2022  October 2022
- ● Coordinated maternity leave transition with school, history department, and administration to reach objectives for autumn semester within a limited time frame.
-● Participated in faculty meetings involving school, department, and specific student goals by providing data gathered from an independently managed classroom.
+Clearview Regional School District, Clearview, NJ August 2022 - October 2022 Long-Term Substitute 
 ● Resolved conflicts between students while collaborating with professionals in guidance and discipline departments.
 Florence Township School District, Florence, NJ September 2021 – May 2022 Student Intern
 ● Observed the practices of established teachers daily. Adapted this knowledge to personal methods.
 ● Participated in weekly meetings with mentors with methodical reflection on lessons and best practices.
 ● Developed materials and implemented lesson plans for 3 different classes, 5 days a week. Combined unique
 ideas with researched solutions from creative and academic sources.
-● Maintained classroom discipline with behavioral modeling for up to 28 students.
-● Analyzed student and school-wide data in order to adjust lessons and reach the goals of the school district.
 `
 const benGPT = `"Yo are BenGPT, a chatbot made to answer questions about Bennett Wilbert. 
 Here is his background info: `
-// const funFacts = `
-// Ben's favorite c
-// `
-const context = benGPT + resume;
-console.log(context);
+const funFacts = `
+In the long term, Ben dreams of being a machine learning engineer 
+Ben's strengths are his positive outlook, tenacity, and willingness to take initiative
+`
+
 
 export default {
   async response(userInput) {
-    const context = benGPT + resume;
-console.log(typeof context);
+    const context = benGPT + resume + funFacts;
     const promptPrimer = {
       model: "gpt-3.5-turbo",
       messages: [{ role: "user", content: context + "Please respond appropriately to the following message:  " + userInput}]
